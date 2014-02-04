7b7d46397b0616b092cc6642a8adc4d5808b346f
jleclaire15 <jleclaire15@punahou.edu>
2014-02-03 14:05:04 -1000

    Commit 1
    Added draft of method that will place first 6 words into an arraylist.
    Added sample documents for testing program.

---
+++ fileToArraylist.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class fileToArraylist throw FileNotFoundException{
+  
+    File f = new File("/Users/jleclaire15/Plagiarism Checker/sm_doc_set 2/catchmeifyoucan.txt");
+  
+    Scanner scan = new Scanner(f);
+    
+    
+}
