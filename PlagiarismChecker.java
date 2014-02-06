35b63050c273a8987e06dbe7e51c5f4c6f476aa9
jleclaire15 <jleclaire15@punahou.edu>
2014-02-05 14:30:18 -1000

    Commit 2
    fileToArraylist method now adds all the words in the first 6 lines of
    catchmeifyoucan.txt in ArrayList b. Will change how many words are
    added and the name of the array in following commit.

--- PlagiarismChecker.java
+++ PlagiarismChecker.java
@@ -5,32 +5,37 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 
 public class PlagiarismChecker {
   
-  public static void main(String[] args)
+  public static void main(String[] args) throws FileNotFoundException
   {
-      
+    
+    ArrayList<String> b = new ArrayList<String>();
+    
+    b.addAll(fileToArraylist());
+
+    System.out.println(b);
+    
     
   }
-  public ArrayList<String> fileToArraylist()  throws FileNotFoundException {
-    
-    ArrayList<String> phrase = new ArrayList<String>();
   
-    File f = new File("/Users/jleclaire15/Plagiarism Checker/sm_doc_set 2/catchmeifyoucan.txt");
-  
-    Scanner scan = new Scanner(f);
+  public static ArrayList<String> fileToArraylist()  throws FileNotFoundException {
     
-    for(int i = 0; i < 6; i++){
+    ArrayList<String> phrase = new ArrayList<String>();  
+    File text = new File("/Users/jleclaire15/Plagiarism Checker/sm_doc_set 2/catchmeifyoucan.txt");
+    Scanner scan = new Scanner(text);    
+    
+    for(int i = 0; i < 6; i++){     
       
-      phrase.add(scan.nextLine());
+       String[] list = scan.nextLine().split(" ");
+    ArrayList<String> list2 = new ArrayList<String>(Arrays.asList(list));
       
+      phrase.addAll(list2);     
     }
     
-
     return phrase;
-
-    
     
   }
 }
