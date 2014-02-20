070699a5659a929d8f42a7518f65aefc863aa264
jleclaire15 <jleclaire15@punahou.edu>
2014-02-19 15:03:59 -1000

    Commit 4
    Added hash/checksum method, still overflows with 1000+ document set.
    Now it compares much smaller hashes instead of 6 word strings. Should
    be faster. Will work on more features such as comparing all documents
    with each other instead of just one document.

--- PlagiarismChecker.java
+++ PlagiarismChecker.java
@@ -1,4 +1,4 @@
-//by Troy Carson
+//John LeClaire, Plagiarism Checker
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -12,62 +12,41 @@ public class PlagiarismChecker {
   
   String filePath = "";
   
-  public PlagiarismChecker(String userFilePath){
+  public PlagiarismChecker(String userFilePath){ //constructor
     filePath = userFilePath; 
   }
   
-  public ArrayList<String> fileToArraylist()  throws FileNotFoundException {    //Arraylist of 6 word phrases
+  public ArrayList<String> fileToArraylist()  throws FileNotFoundException {    //makes arraylist from a filepath
     ArrayList<String> phrase = new ArrayList<String>();  
     
     File text = new File(filePath);
     Scanner scan = new Scanner(text);  
     
-    for(int i = 0; scan.hasNextLine(); i++){       
-      String[] list = scan.nextLine().split(" ");
+    for(int i = 0; scan.hasNextLine(); i++){       //for every line, split into each word, add each word into a string array
+      String[] list = scan.nextLine().split(" ");  //////this method gets an array with a single word in every index///////
       ArrayList<String> list2 = new ArrayList<String>(Arrays.asList(list));
       phrase.addAll(list2);   
     }    
     
-    ArrayList<String> sixwords = new ArrayList<String>();
+    ArrayList<String> sixwords = new ArrayList<String>(); //separate array for 6 word phrase array
     
-    while(phrase.size() >=6){
+    while(phrase.size() >=6){ //adds 0-5 words to make a phrase, adds to sep 6 word array, deletes index 0 from single word array
       sixwords.add(phrase.get(0) + " " + phrase.get(1) + " " + phrase.get(2) + " " + phrase.get(3) + " " + phrase.get(4) + " " + phrase.get(5));
       phrase.remove(0);
-      
     }    
     return sixwords;      
   }
   
-  
-  
-  
-  
-  
-  
-  
-  /*public ArrayList<Integer> listToHash(ArrayList<String> words){
+  public ArrayList<Integer> listToHash(ArrayList<String> words){ //turns arraylist of strings into arraylist of hashes type Integer
     
     ArrayList<Integer> hashlist = new ArrayList<Integer>();
     ArrayList<String> wordlist = words;
-
-    for(String phrase : wordlist){
+    
+    for(String phrase : wordlist){ //for every string, add string.hashCode into Integer arraylist
       
-      hashlist.a
+      hashlist.add(phrase.hashCode());
       
     }
-
-        
-      
-      
-      
-      return null;
-      
-    }
-    
-    
-    
-    
-    
+    return hashlist;      
   }
-  }*/
 }
--- PlagiarismCheckerDriver.java
+++ PlagiarismCheckerDriver.java
@@ -1,4 +1,4 @@
-//by Troy Carson
+//John LeClaire, Plagiarism Checker
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -14,56 +14,44 @@ public class PlagiarismCheckerDriver {
   {
     
     /*
+     * Files to check
      * ./sm_doc_set 2/catchmeifyoucan.txt
-     * ./sm_doc_set 2/edo14.txt
      * 
      */
     
-    //makes file of folder with sample docs
+    File docs = new File("./sm_doc_set 2/"); //makes file of folder with sample docs
     
-    File docs = new File("./sm_doc_set 2/");    
+    ArrayList<ArrayList<String>> wordlist = new ArrayList<ArrayList<String>>(); //array of string arrays to hold wordlists of sample docs  
+    ArrayList<ArrayList<Integer>> hashlist = new ArrayList<ArrayList<Integer>>();
+    ArrayList<PlagiarismChecker> classes = new ArrayList<PlagiarismChecker>(); //array of checker classes to have a different class for each sample doc    
     
-    //makes array of string arrays to hold wordlists of sample docs
-    
-    ArrayList<ArrayList<String>> wordlist = new ArrayList<ArrayList<String>>();
-    
-    //makes array of plagiarismchecker classes to have a different class for each sample doc
-    
-    ArrayList<PlagiarismChecker> classes = new ArrayList<PlagiarismChecker>();
-    
-    //for every samp doc, instantiate a checker and add it to array of checkers
-    
-    for(File doc : docs.listFiles()){      
+    for(File doc : docs.listFiles()){ //for every samp doc, instantiate a checker and add it to array of checkers
       classes.add(new PlagiarismChecker(doc.getPath()));
     }
     
-    //for each one of those checkers, add the sample docs string array to wordlist
-    
-    for(PlagiarismChecker checker : classes){    
-      wordlist.add(checker.fileToArraylist());       
+    for(PlagiarismChecker checker : classes){ //for each one of those checkers, add the sample docs string array to wordlist
+      wordlist.add(checker.fileToArraylist());
+      hashlist.add(checker.listToHash(checker.fileToArraylist())); //turn string array to hashlist
     }
     
     Scanner kb = new Scanner(System.in);
     System.out.println("Enter file path of the document you wish to check.");
-    String filePath1 = kb.nextLine();
+    String filePath1 = kb.nextLine(); //filepath of user doc
     
-    //checker for the user doc
+    PlagiarismChecker checker1 = new PlagiarismChecker(filePath1); //checker for the user doc
+    ArrayList<String> wordlist1 = new ArrayList<String>(); //wordlist, hashlist ofr user doc
+    ArrayList<Integer> hashlist1 = new ArrayList<Integer>();
     
-    PlagiarismChecker checker1 = new PlagiarismChecker(filePath1);  
-    ArrayList<String> wordlist1 = new ArrayList<String>();    
     wordlist1.addAll(checker1.fileToArraylist());
+    hashlist1.addAll(checker1.listToHash(checker1.fileToArraylist()));
     
-    //retain all words same between user doc and sample doc
-    
-    for(ArrayList<String> check : wordlist){
-      check.retainAll(wordlist1);     
+    for(ArrayList<Integer> check : hashlist){ //retain all hashes same between user doc and sample doc
+      check.retainAll(hashlist1);     
     }
     
-    //prints length of all arrays  
-    
-    for(int i = 0; i < docs.listFiles().length; i++){   
+    for(int i = 0; i < docs.listFiles().length; i++){     //prints name and length of all file hashlists  
       System.out.print("six word phrases in common with " + docs.listFiles()[i]);
-      System.out.println("             " + wordlist.get(i).size());  
+      System.out.println("             " + hashlist.get(i).size());  
     }
   }  
 }
